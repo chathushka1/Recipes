2f747db13ca4d1de2a03bc6fcd21e161bab12df7
chathushka1 <www.chathushkamadumal123@gmail.com>
2024-07-21 01:39:08 +0530

    create RecipeList js

--- Recipes/src/navigation/AppNavigator.js
+++ Recipes/src/navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import WelcomeScreen from '../view/WelcomeScreen';
+import WelcomeScreenView from '../view/RecipeList';
 import {createNativeStackNavigator} from "react-native-screens/native-stack";
 
 const Stack = createNativeStackNavigator();
@@ -11,6 +12,7 @@ const AppNavigator = () =>{
             <Stack.Navigator screenOptions={{
                 headerShown:false}}>
                 <Stack.Screen name="Welcome" component={WelcomeScreen}></Stack.Screen>
+                <Stack.Screen name="RecipeListView" component={}></Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
     )
