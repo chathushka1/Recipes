d0caafec6b80395f6cecb5dfaf300aaaf1bb93e0
chathushka1 <www.chathushkamadumal123@gmail.com>
2024-07-21 00:20:45 +0530

    update app js

--- Recipes/src/view/WelcomeScreen.js
+++ Recipes/src/view/WelcomeScreen.js
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
 const WelcomeScreen = () =>{
     return (
-        <View>
-            <Text>WelcomeScreen</Text>
+        <View style={{ flex: 1}}>
+            {/*<Text>WelcomeScreen</Text>*/}
+            <Image source={require("../../assets/images/Welcome.jpg")}></Image>
         </View>
     );
 };
