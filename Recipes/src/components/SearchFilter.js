147e553441603058cbc302851b764da0390026f6
chathushka1 <www.chathushkamadumal123@gmail.com>
2024-07-21 15:04:32 +0530

    update search filter

---
+++ Recipes/src/components/SearchFilter.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+
+const SearchFilter =({icon, placeholder}) =>{
+    return(
+      <View>
+          <Text>Search Filter</Text>
+      </View>
+    );
+};
+
+export default SearchFilter;
+
+const styles = StyleSheet.create({});
