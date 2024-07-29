50483eae361edcd32bccfb11f2fab5675947d06a
chathushka1 <www.chathushkamadumal123@gmail.com>
2024-07-29 11:02:39 +0530

    onPress work done

--- Recipes/src/view/RecipeDetails.js
+++ Recipes/src/view/RecipeDetails.js
@@ -1,10 +1,30 @@
-import {StyleSheet, Text, View} from "react-native";
+import {Image, StyleSheet, Text, View} from "react-native";
 import React from "react";
 
 const RecipeDetails = () =>{
     return (
-        <View>
-            <Text>HI</Text>
+        <View style={{backgroundColor:"#6f4e37", flex: 1}}>
+            <View
+                style={{
+                    backgroundColor:"#fff",
+                    flex: 1,
+                    marginTop:240,
+                    borderTopLeftRadius:56,
+                    borderTopRightRadius:56,
+                }}>
+                <View style={{
+                    //backgroundColor:"red",
+                    height:300,
+                    width:300,
+                    position:"absolute",
+                    top:-150,
+                    left:30,
+                }}>
+                    <Image
+                        source={require('../../assets/images/HotDog.jpg')}
+                        style={{width:"100%", height:"100%", resizeMode:"contain"}}/>
+                </View>
+            </View>
         </View>
     );
 };
